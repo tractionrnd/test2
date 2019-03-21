f0bb391587aa565dc6cfa69765e2df6df966e758
Alex Kovalev <sanyakovalev@gmail.com>
2019-03-21 13:52:46 -0700

    test

--- tcrm_dynamics_integrate/models/dynamics.py
+++ tcrm_dynamics_integrate/models/dynamics.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 
 class Dynamics(models.Model):
 	_name = 'tcrm_dynamics_integrate.dynamics'
-	_inherit = ['mail.alias.mixin']
+	_inherit = ['mail.thread']
 	
 	x_name = fields.Text(
 		string=u'Name',
