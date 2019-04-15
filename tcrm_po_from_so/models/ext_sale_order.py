d6722b0d4c31ed78800b8329df757e0a71950a31
Alex Kovalev <sanyakovalev@gmail.com>
2019-04-15 17:17:10 -0400

    upd 1

--- tcrm_po_from_so/models/ext_sale_order.py
+++ tcrm_po_from_so/models/ext_sale_order.py
@@ -6,4 +6,28 @@ class ExtCRMLead(models.Model):
 
     @api.multi
     def generate_po_from_so(self):
-        test = None
+        poModel = self.env['purchase.order']
+
+        poItem = poModel.create({
+            'date_order': self.confirmation_date
+        })
+
+        #order_line
+        #order_line
+        poLineModel = self.env['purchase.order.line']
+        for line_item in self.order_line:
+            poLineModel.cereate({
+                'order_id': poItem.id,
+                'product_id': line_item.product_id,
+                'product_qty': line_item.product_uom_qty,
+                'product_uom': line_item.product_uom,
+                'date_planned': line_item.create_date
+            })
+
+        return {
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'purchase.order',
+            'target': 'current',
+            'res_id': poItem.id,
+            'type': 'ir.actions.act_window' }
