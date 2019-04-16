65f9021866dbc602853b1b589d718eb0cb292feb
Alex Kovalev <sanyakovalev@gmail.com>
2019-04-16 03:42:14 -0400

    small upd to work with v11

--- tcrm_dynamics_integrate/__manifest__.py
+++ tcrm_dynamics_integrate/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Dynamics - ODOO integration",
+    'name': "Traction - Dynamics integration",
 
     'summary': "",
 
@@ -13,7 +13,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.39',
+    'version': '0.40',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'contacts', 'base_automation', 'crm'],
