8931f841f342b67764f5188eca639aba1547b752
Alex Kovalev <sanyakovalev@gmail.com>
2019-04-16 09:45:46 -0400

    upd 17

--- tcrm_po_from_so/__manifest__.py
+++ tcrm_po_from_so/__manifest__.py
@@ -13,7 +13,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.17',
+    'version': '0.18',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale_management', 'purchase'],
