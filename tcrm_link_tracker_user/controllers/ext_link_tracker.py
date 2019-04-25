7c0ab15c1eca0fd7cc78473255ba3fe7fe82dabb
Alex Kovalev <sanyakovalev@gmail.com>
2019-04-25 16:28:09 -0400

    lnk 0.13

--- tcrm_link_tracker_user/controllers/ext_link_tracker.py
+++ tcrm_link_tracker_user/controllers/ext_link_tracker.py
@@ -4,7 +4,7 @@ import werkzeug
 from odoo import http
 from odoo.http import request
 
-class ExtLinkTracker(LinkTracker):
+class LinkTracker(LinkTracker):
     @http.route('/r/<string:code>', type='http', auth='none', website=True)
     def full_url_redirect(self, code, **post):
         country_code = request.session.geoip and request.session.geoip.get('country_code') or False
