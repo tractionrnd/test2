193567129becab427b0ad50c1b78bbf10531b848
Alex Kovalev <sanyakovalev@gmail.com>
2019-04-25 10:27:48 -0400

    lnk 0.1

---
+++ tcrm_link_tracker_user/models/ext_link_tracker_click.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class ExtLinkTrackerClick(models.Model):
+    _inherit = 'link.tracker.click'
+
+    x_user_name = fields.Char(
+        string=u'User Name',
+    )
+
+    x_user_email = fields.Char(
+        string=u'User Email',
+    )
+
+    @api.model
+    def add_click(self, code, ip, country_code, stat_id=False):
+        self = self.sudo()
+        code_rec = self.env['link.tracker.code'].search([('code', '=', code)])
+
+        if not code_rec:
+            return None
+
+        again = self.search_count([('link_id', '=', code_rec.link_id.id), ('ip', '=', ip)])
+
+        if not again:
+            self.create(
+                super(ExtLinkTrackerClick, self)._get_click_values_from_route(dict(
+                    code=code,
+                    ip=ip,
+                    country_code=country_code,
+                    stat_id=stat_id,
+                )))
