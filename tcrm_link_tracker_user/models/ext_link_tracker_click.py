0046d6bf24e95b1e2c85bb5c9cbb0946a75ec393
Alex Kovalev <sanyakovalev@gmail.com>
2019-04-25 15:28:40 -0400

    lnk 0.11

--- tcrm_link_tracker_user/models/ext_link_tracker_click.py
+++ tcrm_link_tracker_user/models/ext_link_tracker_click.py
@@ -12,6 +12,26 @@ class ExtLinkTrackerClick(models.Model):
         string=u'User Email',
     )
 
+    @api.model
+    def add_click(self, code, ip, country_code, stat_id=False, uid):
+        self = self.sudo()
+        code_rec = self.env['link.tracker.code'].search([('code', '=', code)])
+
+        if not code_rec:
+            return None
+
+        again = self.search_count([('link_id', '=', code_rec.link_id.id), ('ip', '=', ip)])
+
+        if not again:
+            self.create(
+                self._get_click_values_from_route(dict(
+                    code=code,
+                    ip=ip,
+                    country_code=country_code,
+                    stat_id=stat_id,
+                    uid=uid
+                )))
+
     def _get_click_values_from_route(self, route_values):
         data = super(ExtLinkTrackerClick, self)._get_click_values_from_route(route_values)
 
@@ -22,8 +42,8 @@ class ExtLinkTrackerClick(models.Model):
             uemail = self.env.user.email
 
         data.update({
-            'x_user_name': uname, #route_values['uname'],
-            'x_user_email': uemail #route_values['uemail']
+            'x_user_name': route_values['uid'],
+            'x_user_email': route_values['uid']
         })
 
         return data
