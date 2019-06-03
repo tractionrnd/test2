0fe85a94ed812e289dedc0c7c6f3500e104aff01
tractionrnd <42620756+tractionrnd@users.noreply.github.com>
2019-06-03 13:33:12 -0400

    test2

--- tcrm_mass_mailing_contact_score/__manifest__.py
+++ tcrm_mass_mailing_contact_score/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "Traction - Mass Mailing Contact Score",
 
-    'summary': "",
+    'summary': "Adds necessary fields for Odoo - Dynamics sync",
 
     'description': "",
 
@@ -13,7 +13,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.6',
+    'version': '0.7',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'mass_mailing', 'base_automation', 'marketing_automation'],
