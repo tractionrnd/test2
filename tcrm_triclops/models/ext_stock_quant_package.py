d2cb4f4c14108b211e83733271bce5ccedfbfd56
Alex Kovalev <sanyakovalev@gmail.com>
2019-06-25 05:48:46 -0400

    implemented integration with Tricolops REST API

--- tcrm_triclops/models/ext_stock_quant_package.py
+++ tcrm_triclops/models/ext_stock_quant_package.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
+import json
+import requests
+from odoo.exceptions import ValidationError
 
 class ExtStockQuantPackage(models.Model):
     _inherit = 'stock.quant.package'
@@ -7,16 +10,27 @@ class ExtStockQuantPackage(models.Model):
     @api.multi
     def get_weight(self):
 
-        package_type = self.env['product.packaging'].create({
-            'name': 'Test Name',
-            'package_carrier_type': 'none',
-            'height': 0,
-            'width': 0,
-            'length': 1,
-            'max_weight': 1.00,
-            'barcode': 'barcode',
-            'shipper_package_code': 'package code'
-        })
+        webserverUrl = self.env['ir.config_parameter'].sudo().get_param('tcrm_triclops.x_tricolops_webserverl') or 'http://localhost:8080/data'
+        response = requests.get(webserverUrl)
 
-        self.packaging_id = package_type.id
-        self.shipping_weight = 10
+        if response.status_code == 200:
+            jsonResponse = response.json()
+
+            if jsonResponse['status'] != 7:
+                raise ValidationError(jsonResponse['message'])
+
+            package_type = self.env['product.packaging'].create({
+                'name': self.name,
+                'package_carrier_type': 'none',
+                'height': jsonResponse['height'],
+                'width': jsonResponse['width'],
+                'length': jsonResponse['length'],
+                'max_weight': jsonResponse['weight']#,
+                #'barcode': 'barcode',
+                #'shipper_package_code': 'package code'
+            })
+
+            self.packaging_id = package_type.id
+            self.shipping_weight = jsonResponse['weight']
+        else:
+            raise ValidationError('Server not responded')
